ce2811a6af4b6fd93b3879a4be4595812bfd4fa1
NathanWigen <nathan.s.wigen@hotmail.com>
2022-01-27 12:10:02 -0800

    finished project.

---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "../UI/Button.module.css";
+
+export default function Button(props) {
+  return (
+    <button
+      className={classes.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+}
